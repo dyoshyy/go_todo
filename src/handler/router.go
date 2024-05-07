1c05b0328140908619e482d0fcec3ec2c508c209
dyoshy <d.yoshhyy@gmail.com>
2024-05-07 20:52:21 +0900

    feat: handler

---
+++ src/handler/router.go
@@ -0,0 +1,16 @@
+package handler
+
+import (
+	"github.com/labstack/echo"
+)
+
+func InitRouting(e *echo.Echo, todoHandler TodoHandler) {
+
+	e.GET("/", todoHandler.View())
+
+	e.GET("/search", todoHandler.Search())
+
+	e.POST("/create", todoHandler.Add())
+
+	e.POST("/edit", todoHandler.Edit())
+}
